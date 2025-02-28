48b36292e97c11d74dd8593f447142b28e1d5046
Oscar Miranda <renemirandau4@gmail.com>
2025-02-27 23:20:42 -0600

    first trying to deploy emplify by only using cdk

--- lib/amplify-dev-opz-stack.ts
+++ lib/amplify-dev-opz-stack.ts
@@ -1,43 +1,34 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import * as amplify from "@aws-cdk/aws-amplify-alpha"
-import * as sm from "aws-cdk-lib/aws-secretsmanager"
-import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
-import * as path from 'path'
-import { Code, Handler, Runtime } from 'aws-cdk-lib/aws-lambda';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as amplify from "@aws-cdk/aws-amplify-alpha";
+import {
+  SecretsManagerClient,
+  GetSecretValueCommand,
+} from "@aws-sdk/client-secrets-manager";
 
-export class AmplifyDevOpzStack extends cdk.Stack {
+export class  AmplifyFrontendDeploymentStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
 
-    const secret = sm.Secret.fromSecretAttributes(this, "ImportedSecret", {
-      secretCompleteArn:
-        "arn:aws:secretsmanager:us-east-1:179086988825:secret:prod/amplify/token-gyw4Jn"
-      // If the secret is encrypted using a KMS-hosted CMK, either import or reference that key:
-      // encryptionKey: ...
-    });
-
-    console.log(secret.secretValue)
-
-    const testingLAmbda = new NodejsFunction(this, 'testingLambda', {
-      runtime: Runtime.NODEJS_18_X,
-      functionName:'testingLambda',
-      handler: 'handler.handler',
-      code: Code.fromAsset(path.join(__dirname, "../dist/")),
-      environment:{
-        SECRET_VALUE: secret.secretValue.toString()
-      }
-    })
-    /*
-    const amplifyApp = new amplify.App(this, 'MyApp', {
-      sourceCodeProvider: new amplify.GitLabSourceCodeProvider({
-        owner: '<user>',
-        repository: '<repo>',
-        oauthToken: secret.secretValue,
+    // Create an Amplify App
+    const amplifyApp = new amplify.App(this, "FrontendApp", {
+      sourceCodeProvider: new amplify.GitHubSourceCodeProvider({
+        owner: "Rene31416",
+        repository: "dev-opz-frontend",
+        oauthToken: cdk.SecretValue.secretsManager('prod/amplify/token'),
       }),
     });
-    */
+
+    console.log(cdk.SecretValue.secretsManager('prod/amplify/token'))
+
+    // Add a branch to deploy
+    const mainBranch = amplifyApp.addBranch("main");
+
+    // Output the Amplify URL after deployment
+    new cdk.CfnOutput(this, "AmplifyAppUrl", {
+      value: `https://${mainBranch.branchName}.${amplifyApp.appId}.amplifyapp.com`,
+      description: "Amplify Hosted Frontend URL",
+    });
   }
 }
